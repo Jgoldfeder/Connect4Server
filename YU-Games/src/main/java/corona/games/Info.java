91cd19482c30b46fa7d28d82aee600801dcac3e4
danielschaffel <deschaffel97@gmail.com>
2020-03-27 17:01:31 -0400

    Restructed with maven

---
+++ YU-Games/src/main/java/corona/games/Info.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+class Info implements Serializable{
+    int color=0;
+    int x=0;
+    int y=0;
+    boolean exit = false;
+    boolean move = false;
+    int ID = -1;
+}
