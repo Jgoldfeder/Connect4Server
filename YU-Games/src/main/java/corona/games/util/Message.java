53fc66fc5ba4794d71f422eaf77a984e0e1c433e
jgoldfeder <jgoldfed@mail.yu.edu>
2020-03-29 18:29:35 -0400

    added msg field

--- YU-Games/src/main/java/corona/games/util/Message.java
+++ YU-Games/src/main/java/corona/games/util/Message.java
@@ -1,6 +1,8 @@
 package corona.games.util;
 public class Message {
-    public enum MessageType {
+    public static enum MessageType {
         INIT_CLIENT, INIT_RESPONSE, CHAT_MSG, EXIT;
     }
+    long clientID = 0;
+    String msg = null;
 }
