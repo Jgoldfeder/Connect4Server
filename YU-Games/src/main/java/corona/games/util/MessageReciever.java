5d7d82780b8dcbe53fa7030517624ac4749429c7
danielschaffel <deschaffel97@gmail.com>
2020-03-29 20:11:54 -0400

    Basic Messagehandler classes should now be working

--- YU-Games/src/main/java/corona/games/util/MessageReciever.java
+++ YU-Games/src/main/java/corona/games/util/MessageReciever.java
@@ -1,24 +1,50 @@
 package corona.games.util;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.Socket;
 import java.util.concurrent.LinkedBlockingDeque;
 
 import corona.games.util.Message;
 
-public class MessageReciever implements Runnable{
+public class MessageReciever implements Runnable {
 
-    
+    private Socket socket;
     private LinkedBlockingDeque<Message> incomingMessages;
-    private volatile int shutdown = 0;
+    private volatile boolean shutdown = false;
 
-    public MessageReciever(InputStream in,LinkedBlockingDeque<Message> incomingMessages) {
-        this.in = in;
+    public MessageReciever(Socket s, LinkedBlockingDeque<Message> incomingMessages) {
+        this.socket = s;
         this.incomingMessages = incomingMessages;
     }
+
+    public void shutdown() {
+        this.shutdown = true;
+    }
+    
     @Override
     public void run() {
         // TODO Auto-generated method stub
+        while (!shutdown) {
+            if (socket.isConnected()) {
+                // This number is kinda random have to really fine tune ideal message size
+                byte[] buffer = new byte[40960];
+                int i;
+                try {
+                    i = socket.getInputStream().read(buffer);
+                    if(i > 1) {
+                        incomingMessages.put(new Message(buffer));
+                    }
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
 
+            }
+        }
     }
 
 }
--- YU-Games/src/main/java/corona/games/util/MessageSender.java
+++ YU-Games/src/main/java/corona/games/util/MessageSender.java
@@ -1,10 +1,43 @@
 package corona.games.util;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+
 public class MessageSender implements Runnable {
+
+    private Socket socket;
+    private LinkedBlockingDeque<Message> outgoingMessages;
+    private volatile boolean shutdown = false;
+
+    public MessageSender(Socket s, LinkedBlockingDeque<Message> outgoingMessages) {
+        this.socket = s;
+        this.outgoingMessages = outgoingMessages;
+    }
 
     @Override
     public void run() {
         // TODO Auto-generated method stub
+        while (!shutdown) {
+            Message m = null;
+            try {
+                m = outgoingMessages.poll(1, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            if (m != null) {
+                try {
+                    socket.getOutputStream().write(m.getNetworkPayload());
+                    socket.getOutputStream().flush();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 }
