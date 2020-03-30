19890956144ddd2fd9bfc2d892d4e11d59966569
danielschaffel <deschaffel97@gmail.com>
2020-03-29 20:20:08 -0400

    added message handlers and queues in the Client

--- YU-Games/src/main/java/corona/games/client/Client.java
+++ YU-Games/src/main/java/corona/games/client/Client.java
@@ -3,38 +3,59 @@ package corona.games.client;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.concurrent.LinkedBlockingDeque;
+
+import corona.games.util.*;
 
 public class Client implements Runnable {
 
-    private volatile int shutdown = 0;
+    private volatile boolean shutdown = false;
 
     private final String host;
     private final int port;
     private Socket socket;
 
-    
     private MessageReciever receiver;
     private MessageSender sender;
+
+    private LinkedBlockingDeque<Message> outgoingMessages;
+    private LinkedBlockingDeque<Message> incomingMessages;
 
     public Client(String hostName, int port) {
         this.host = hostName;
         this.port = port;
+
+        this.outgoingMessages = new LinkedBlockingDeque<>();
+        this.incomingMessages = new LinkedBlockingDeque<>();
     }
 
     @Override
     public void run() {
 
         try {
-            socket = new Socket(this.host, this.port);
-        } catch (UnknownHostException  e) {
+            this.socket = new Socket(this.host, this.port);
+        } catch (UnknownHostException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        sender = new MessageSender(this.socket, this.outgoingMessages);
+        receiver = new MessageReciever(this.socket, this.incomingMessages);
+
+        while(!shutdown) {
+
+        }
 
     }
-    
-    
+
+    public void sendMessage(Message m) {
+        try {
+            this.outgoingMessages.put(m);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
