5ad8d26bafd1ea9c45b2253dedb01b7506bec8e6
Noam Annenberg <noam.annenberg@gmail.com>
2020-04-05 12:44:05 -0400

    added GameInfo fields

--- src/main/java/corona/games/communication/GameInfo.java
+++ src/main/java/corona/games/communication/GameInfo.java
@@ -5,4 +5,13 @@ package corona.games.communication;
  * on 4/5/20.
  */
 public class GameInfo {
+    public final String name;
+    public final int minPlayers;
+    public final int maxPlayers;
+
+    public GameInfo(String name, int minPlayers, int maxPlayers) {
+        this.name = name;
+        this.minPlayers = minPlayers;
+        this.maxPlayers = maxPlayers;
+    }
 }
